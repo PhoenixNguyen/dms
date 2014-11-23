bb0b359af7b29113fc6718c14e300148165859ad
HP <hpintro@gmail.com>
2014-11-23 14:54:22 +0700

    export for-leave

--- DMSServer/src/java/com/hp/action/ForLeaveAction.java
+++ DMSServer/src/java/com/hp/action/ForLeaveAction.java
@@ -18,11 +18,20 @@ import com.hp.dao.UserDAO;
 import com.hp.dao.UserDAOImpl;
 import com.hp.domain.ForLeave;
 import com.hp.domain.PushInfo;
+import com.hp.domain.TimeKeeper;
 import com.hp.domain.User;
+import static com.opensymphony.xwork2.Action.INPUT;
 import static com.opensymphony.xwork2.Action.LOGIN;
 import static com.opensymphony.xwork2.Action.SUCCESS;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -30,6 +39,14 @@ import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.CellRangeAddress;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.struts2.ServletActionContext;
 
 /**
@@ -55,6 +72,9 @@ public class ForLeaveAction extends ActionSupport{
     
     private String startDate;
     private String endDate;
+    
+    public FileInputStream orderFile;
+    String outputFile;
     
     public String displayForLeave(){
         HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
@@ -114,8 +134,202 @@ public class ForLeaveAction extends ActionSupport{
         forLeaveList = forLeaveDAO.getForLeaveList(pushInfo.getManagerID(), pushInfo.getStaffID(), 
                 start, end);
         
+        session.setAttribute("forLeaveList", forLeaveList);
+        session.setAttribute("startDate", startDate);
+        session.setAttribute("endDate", endDate);
+        
         return SUCCESS;
     }
+    
+    public String exportExcel(){
+        HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
+        HttpSession session = request.getSession();
+        
+        user = (User)session.getAttribute("USER");
+        
+        //Authorize
+        if(!userDAO.authorize((String)session.getAttribute("user_name"), (String)session.getAttribute("user_password")) ){
+            return LOGIN;
+        }
+        
+        //GET DATA
+        forLeaveList = (List<ForLeave>)session.getAttribute("forLeaveList");
+        
+        if(forLeaveList == null)
+            return INPUT;
+        
+        String fileInput = ServletActionContext.getServletContext().getRealPath("/db_exports/");
+        String start = (String)session.getAttribute("startDate");
+        String end = (String)session.getAttribute("endDate");
+
+        //
+        //Write
+        HSSFWorkbook workBook = new HSSFWorkbook();
+        HSSFSheet sheet = workBook.createSheet("Nghỉ phép");
+        //sheet.autoSizeColumn(200);
+        sheet.setColumnWidth(0, 1000);
+        sheet.setDefaultColumnWidth(20);
+        
+        //TakeOrder title
+        for(int i = 1; i < 2; i++){
+            //
+            Row rowstart = sheet.createRow(0);
+            
+            //Row Title
+            Row row0 = sheet.createRow(i);
+            row0.setHeight((short)500);
+            Cell cell0 = row0.createCell(0);
+            
+            //Merge for title
+            sheet.addMergedRegion(new CellRangeAddress(
+                    i, //first row (0-based)
+                    i, //last row  (0-based)
+                    0, //first column (0-based)
+                    5  //last column  (0-based)
+            ));
+            //CellUtil.setAlignment(cell0, workBook, CellStyle.ALIGN_CENTER);
+            CellStyle cellStyle = workBook.createCellStyle();
+            cellStyle.setAlignment(CellStyle.ALIGN_CENTER);
+            
+            //font
+            Font headerFont = workBook.createFont();
+            headerFont.setBoldweight(Font.BOLDWEIGHT_BOLD);
+            headerFont.setFontHeight((short)250);
+            cellStyle.setFont(headerFont);
+            
+            cell0.setCellStyle(cellStyle);
+            cell0.setCellValue("Báo cáo nghỉ phép");
+            
+            //Row date
+            Row row1 = sheet.createRow(i+1);
+            //row1.setHeight((short)500);
+            Cell cell1 = row1.createCell(0);
+            
+            //Merge for title
+            sheet.addMergedRegion(new CellRangeAddress(
+                    i+1, //first row (0-based)
+                    i+1, //last row  (0-based)
+                    0, //first column (0-based)
+                    5  //last column  (0-based)
+            ));
+            CellStyle cellAlign = workBook.createCellStyle();
+            cellAlign.setAlignment(CellStyle.ALIGN_CENTER);
+            cell1.setCellStyle(cellAlign);
+            
+            
+            if(start == null)
+                start = "";
+            if(end == null)
+                end = "";
+            cell1.setCellValue("Từ ngày: " + start+ " - Đến ngày: " + end);
+            
+            //Row Header
+            Row row = sheet.createRow(4);
+            int cellnum = 0;
+            
+            for(Object obj : titleArray()){
+                Cell cell = row.createCell(cellnum++);
+                
+                CellStyle style = workBook.createCellStyle();
+                style.setFillForegroundColor(HSSFColor.YELLOW.index);
+                style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+
+               cell.setCellStyle(style);
+               
+                if(obj instanceof Timestamp) 
+                    cell.setCellValue((Timestamp)obj);
+                else if(obj instanceof Boolean)
+                    cell.setCellValue((Boolean)obj);
+                else if(obj instanceof String)
+                    cell.setCellValue((String)obj);
+                else if(obj instanceof Float)
+                    cell.setCellValue((Float)obj);
+            }
+            
+            
+            
+        }
+        //Write TakeOrder
+        for(int i = 0; i < forLeaveList.size(); i++){
+            Row row = sheet.createRow(i+5);
+            int cellnum = 0;
+            
+            //Cell 0 - stt
+            Cell cell0 = row.createCell(cellnum++);
+            cell0.setCellValue(i+1);
+           
+            //Set content
+            for(Object obj : objectArray(forLeaveList.get(i))){
+                Cell cell = row.createCell(cellnum++);
+                
+                if(obj instanceof Timestamp) 
+                    cell.setCellValue((Timestamp)obj);
+                else if(obj instanceof Boolean)
+                    cell.setCellValue((Boolean)obj);
+                else if(obj instanceof Integer)
+                    cell.setCellValue((Integer)obj);
+                else if(obj instanceof String)
+                    cell.setCellValue((String)obj);
+                else if(obj instanceof Float){
+                    
+//                    CellStyle cellStyle = workBook.createCellStyle();
+//                    DataFormat format = workBook.createDataFormat();
+//                    cellStyle.setDataFormat(format.getFormat("#.#"));
+//                    cell.setCellStyle(cellStyle);
+                    
+                    cell.setCellValue((Float)obj);
+                }
+                else if(obj instanceof Double)
+                    cell.setCellValue((Double)obj);
+            }
+            
+            
+        }
+        
+        outputFile = "BaoCaoNghiPhep"+start+" - "+end+".xls";
+        try{
+            FileOutputStream output = new FileOutputStream(new File(fileInput +"\\" + outputFile));
+            
+            
+            workBook.write(output);
+            output.close();
+            System.out.println("Excel written successfully..");
+            orderFile = new FileInputStream(new File(fileInput +"\\"+outputFile));
+            
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return SUCCESS;
+    }
+
+    public Object[] titleArray(){
+        return new Object[]{
+            "Stt",
+            "Mã nhân viên",
+            "Tên nhân viên",
+            "Ngày ",
+            "Nội dung",
+            "Ngày tạo"
+        };
+    }
+    
+    public Object[] objectArray(ForLeave forLeave){
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat df2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        
+        return new Object[]{
+            forLeave.getStaff().getId(),
+            forLeave.getStaff().getName(),
+            df.format(forLeave.getTimeAt()),
+            forLeave.getContent(),
+            df2.format(forLeave.getCreatedTime())
+                
+        };
+    }
+    
     public List<ForLeave> getForLeaveList() {
         return forLeaveList;
     }
@@ -179,5 +393,21 @@ public class ForLeaveAction extends ActionSupport{
 
     public void setEndDate(String endDate) {
         this.endDate = endDate;
+    }
+    
+    public String getOutputFile() {
+        return outputFile;
+    }
+
+    public void setOutputFile(String outputFile) {
+        this.outputFile = outputFile;
+    }
+    
+    public FileInputStream getOrderFile() {
+        return orderFile;
+    }
+
+    public void setOrderFile(FileInputStream orderFile) {
+        this.orderFile = orderFile;
     }
 }
--- DMSServer/web/view/staff/for-leave.jsp
+++ DMSServer/web/view/staff/for-leave.jsp
@@ -165,9 +165,11 @@
 
 
                             <br>
-                            <table align="center" border="0" cellpadding="5" cellspacing="0" width="100%" class="mailSubHeader">
+                            <table align="center" border="0" cellpadding="5" cellspacing="0" width="100%" class="mailSubHeader" >
                                 <tbody><tr>
-                                        <!--                                        <td align="left" nowrap ><input class="crmbutton small create" id="btnExport" name="btnExport" value="Xuất dữ liệu Excel" type="button" onClick="window.location.href='export-takeorder-xls'" title="Xuất dữ liệu Excel" ></td>-->
+                                        <td align="left" nowrap >
+                                            <input class="crmbutton small create" id="btnExport" name="btnExport" value="Xuất dữ liệu Excel" type="button" onClick="location.href='export-forleave'" title="Xuất dữ liệu Excel" >
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -243,10 +245,11 @@
                                 </tbody>
                             </table>
 
-                            <table align="center" border="0" cellpadding="5" cellspacing="0" width="100%" class="mailSubHeader">
-                                <tbody>
-                                    <tr>
-                                        <!--                    <td align="left" nowrap ><input class="crmbutton small create" id="btnExport" name="btnExport" value="Xuất dữ liệu Excel" type="button" onClick="window.location.href='export-takeorder-xls'" title="Xuất dữ liệu Excel" ></td>-->
+                            <table align="center" border="0" cellpadding="5" cellspacing="0" width="100%" class="mailSubHeader" >
+                                <tbody><tr>
+                                        <td align="left" nowrap >
+                                            <input class="crmbutton small create" id="btnExport" name="btnExport" value="Xuất dữ liệu Excel" type="button" onClick="location.href='export-forleave'" title="Xuất dữ liệu Excel" >
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
